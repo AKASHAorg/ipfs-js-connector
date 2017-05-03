b97269b9fb65b95a23a9aa4fe3ffe871fbb9f8e6
Marius Darila <marius.darila@gmail.com>
2017-05-03 17:41:35 +0300

    refactor(): initial

--- index.ts
+++ index.ts
@@ -1,3 +1,293 @@
+import * as IPFS from 'ipfs';
+import IpfsApiHelper from '@akashaproject/ipfs-connector-utils';
+import * as Promise from 'bluebird';
+
+const symbolEnforcer = Symbol();
+const symbol = Symbol();
+const requiredVersion = '0.23.1';
+
 export default class IpfsJsConnector {
 
+    public apiAddress: string;
+    private _process: any;
+    private _api: any;
+    private _ports = {
+        API: 5042,
+        Gateway: 8042,
+        Swarm: 4042
+    };
+
+    private _options = {
+        repo: '0x497066734a73436f6e6e6563746f72',
+        init: true,
+        start: true,
+        EXPERIMENTAL: {
+            pubsub: true,
+            sharding: true
+        },
+        config: {
+            Addresses: {}
+        }
+    };
+
+    public logger: any = console;
+    public serviceStatus: { api: boolean, process: boolean, version: string } = {
+        process: false,
+        api: false,
+        version: ''
+    };
+
+    /**
+     *
+     * @param enforcer
+     */
+    constructor(enforcer: any) {
+        if (enforcer !== symbolEnforcer) {
+            throw new Error('Use .getInstance() instead of constructing a new object');
+        }
+    }
+
+    /**
+     *
+     * @returns {any}
+     */
+    public static getInstance(): IpfsJsConnector {
+        if (!this[symbol]) {
+            this[symbol] = new IpfsJsConnector(symbolEnforcer);
+        }
+        return this[symbol];
+    }
+
+    /**
+     * Get ipfs-connector-utils instance
+     * @returns {any}
+     */
+    get api() {
+        if (!this._api) {
+            this._requireInstance();
+            this._api = new IpfsApiHelper(this._process);
+        }
+        return this._api;
+    }
+
+    /**
+     *
+     * @param key
+     * @returns {any}
+     */
+    public getOption(key: string) {
+        if (!this._options.hasOwnProperty(key)) {
+            throw new Error(`Option ${key} does not exist.`);
+        }
+        return this._options[key];
+    }
+
+    /**
+     *
+     * @param key
+     * @param value
+     */
+    public setOption(key: string, value: any) {
+        this._options[key] = value;
+    }
+
+    /**
+     * Overwrite all options
+     * @param config
+     */
+    public setOptions(config: any) {
+        this._options = config;
+    }
+
+    /**
+     * Get ipfs start configuration
+     * @returns {{}&{repo: string, init: boolean, start: boolean, EXPERIMENTAL: {pubsub: boolean, sharding: boolean}, config: {Addresses: {}}}&{config: {Addresses: {API: string, Gateway: string, Swarm: ([string,string]|[string])}}}}
+     */
+    public get config() {
+        return Object.assign({},
+            this._options,
+            {
+                config: {
+                    Addresses: {
+                        API: (this._ports.API) ? `/ip4/127.0.0.1/tcp/${this._ports.API}` : '',
+                        Gateway: (this._ports.Gateway) ? `/ip4/127.0.0.1/tcp/${this._ports.Gateway}` : '',
+                        Swarm: (this._ports.Swarm) ? [
+                            `/ip4/0.0.0.1/tcp/${this._ports.Swarm}`,
+                            `/ip6/::/tcp/${this._ports.Swarm}`
+                        ] : ['']
+                    }
+                }
+            }
+        );
+    }
+
+    /**
+     *
+     * @param newLogger
+     */
+    public setLogger(newLogger: object) {
+        this.logger = newLogger;
+    }
+
+    /**
+     * Path to ipfs store
+     * @param path
+     */
+    public setIpfsFolder(path: string) {
+        this._options.repo = path;
+    }
+
+    /**
+     *
+     * @returns {Bluebird}
+     */
+    public start() {
+        return new Promise((resolve, reject) => {
+            this._process = new IPFS(this.config);
+            this.apiAddress = this.config.config.Addresses.API;
+            this.on('error', (err) => {
+                this.logger.error(err);
+                reject(err);
+            });
+            this.on('start', () => {
+                this.serviceStatus.process = true;
+                this.logger.info('js-ipfs started');
+                resolve(this);
+            });
+        }).then(() => {
+            return this.api.apiClient.versionAsync().then((data: any) => {
+                this.serviceStatus.api = true;
+                this.serviceStatus.version = data.version;
+                return this;
+            });
+        });
+    }
+
+    /**
+     *
+     * @returns {any}
+     */
+    public getNode() {
+        return this._process;
+    }
+
+    /**
+     *
+     * @returns {boolean}
+     */
+    public stop() {
+        if (this._process) {
+            this._process.stop();
+        }
+        this._api = null;
+        this._process = null;
+        this.serviceStatus.api = false;
+        this.serviceStatus.process = false;
+        return true;
+    }
+
+    /**
+     * Ex: 'ready', 'error', 'init', 'start', 'stop'
+     * @param event
+     * @param cb
+     * @returns {any}
+     */
+    public on(event: string, cb: (data?: any) => void) {
+        this._requireInstance();
+        return this._process.on(event, cb);
+    }
+
+    /**
+     *
+     * @private
+     */
+    private _requireInstance() {
+        if (!this._process) {
+            throw new Error('Must provide a js-ipfs instance.');
+        }
+    }
+
+    /**
+     *
+     * @param event
+     * @param cb
+     */
+    public once(event: string, cb: (data?: any) => void) {
+        this._requireInstance();
+        return this._process.once(event, cb);
+    }
+
+    /**
+     *
+     * @param event
+     * @param cb
+     */
+    public removeListener(event: string, cb: (data?: any) => void) {
+        this._requireInstance();
+        return this._process.removeListener(event, cb);
+    }
+
+    /**
+     *
+     * @param event
+     * @returns {any|Cluster}
+     */
+    public removeAllListeners(event: string) {
+        this._requireInstance();
+        return this._process.removeAllListeners(event);
+    }
+
+    public listenerCount(event: string) {
+        this._requireInstance();
+        return this._process.listenerCount(event);
+    }
+
+    /**
+     *
+     * @returns {{gateway: number, api: number, swarm: number}}
+     */
+    public getPorts() {
+        return Promise.resolve({
+            gateway: this._ports.Gateway,
+            api: this._ports.API,
+            swarm: this._ports.Swarm
+        });
+    }
+
+    /**
+     *
+     * @param ports
+     * @param restart
+     * @returns {Bluebird<{API, Gateway, Swarm}>}
+     */
+    public setPorts(ports: { gateway?: number, api?: number, swarm?: number }, restart = false) {
+        this._ports = Object.assign({}, this._ports, {
+            Gateway: ports.gateway,
+            API: ports.api,
+            Swarm: ports.swarm
+        });
+
+        if (restart) {
+            return Promise
+                .resolve(this.stop()).delay(1000)
+                .then(() => {
+                    return this.start().delay(500);
+                })
+                .then(() => ports);
+        }
+        return Promise.resolve(ports);
+    }
+
+    /**
+     *
+     * @returns {Bluebird<boolean>|Bluebird<U2|boolean>|PromiseLike<TResult2|boolean>|Thenable<boolean>|PromiseLike<boolean>|Promise<TResult|boolean>|any}
+     */
+    public checkVersion() {
+        return this.api.apiClient.versionAsync().then(
+            (data: any) => {
+                this.serviceStatus.version = data.version;
+                return data.version === requiredVersion;
+            }
+        );
+    }
 }
--- tests.ts
+++ tests.ts
@@ -1,3 +1,163 @@
-describe('ipfs-js-connector', function () {
+import IpfsConnector from './index';
+import { expect } from 'chai';
+import * as rimraf from 'rimraf';
 
+describe('ipfs-js-connector', function () {
+    this.timeout(4000);
+
+    let instance: any;
+    const repo = 'test-repo';
+    const logger = {
+        info: function () {
+        },
+        error: function () {
+        },
+        warn: function () {
+        }
+    };
+
+    it('gets singleton instance', function () {
+        instance = IpfsConnector.getInstance();
+        expect(instance).to.exist;
+    });
+
+    it('prevents multiple instances', function () {
+      const getNewInstance = () => new IpfsConnector(Symbol());
+      expect(getNewInstance).to.throw(Error);
+    });
+
+    it('sets ipfs storage path', function () {
+        IpfsConnector.getInstance().setIpfsFolder(repo);
+        expect(IpfsConnector.getInstance().getOption('repo')).to.equal(repo);
+    });
+    it('constructs configuration for ipfs node', function () {
+        expect(IpfsConnector.getInstance().config).to.have.property('config');
+    });
+
+    it('starts an js-ipfs node', function () {
+        return instance.start().then((node: any) => {
+            expect(node.serviceStatus.process).to.be.true;
+        });
+    });
+
+    it('provides an api helper', function () {
+        expect(instance.api).to.exist;
+    });
+
+    it('throws for unknown option', function () {
+        const validate = () => IpfsConnector.getInstance().getOption('notExistent');
+        expect(validate).to.throw(Error);
+    });
+
+    it('sets a different logger', function () {
+        IpfsConnector.getInstance().setLogger(logger);
+        expect(IpfsConnector.getInstance().logger).to.eql(logger);
+    });
+
+    it('checks current ipfs version', function () {
+        return IpfsConnector.getInstance().checkVersion().then((valid: boolean) => {
+            expect(valid).to.be.true;
+        });
+    });
+
+    it('sets an option', function () {
+        IpfsConnector.getInstance().setOption('init', false);
+        expect(IpfsConnector.getInstance().getOption('init')).to.eql(false);
+    });
+
+    it('stops ipfs node', function () {
+        IpfsConnector.getInstance().stop();
+        expect(IpfsConnector.getInstance().getNode()).to.be.null;
+    });
+
+    it('prevents listening before starting an ipfs node', function () {
+        const listener = () => IpfsConnector.getInstance().on('start', () => {
+        });
+        expect(listener).to.throw(Error);
+    });
+
+    it('can attach listeners', function (done) {
+        IpfsConnector.getInstance().start().then((i: any) => {
+            IpfsConnector.getInstance().once('stop', () => {
+                done();
+            });
+            i.stop();
+        });
+    });
+
+    it('gets listeners count', function () {
+        return IpfsConnector.getInstance().start().then((i: any) => {
+            expect(i.listenerCount('stop')).to.eql(0);
+        });
+    });
+
+    it('gets current ports used', function () {
+        return IpfsConnector.getInstance().getPorts().then((ports) => {
+            expect(ports).to.have.property('api');
+        });
+    });
+
+    it('should set ipfs GATEWAY port', function () {
+        return instance.setPorts({ gateway: 8092 }).then((ports: any) => {
+            expect(ports).to.exist;
+        });
+    });
+
+    it('should set ipfs API port', function () {
+        return instance.setPorts({ api: 5043 }).then((ports: any) => {
+            expect(ports).to.exist;
+        });
+    });
+
+    it('should set ipfs SWARM port', function () {
+        return instance.setPorts({ swarm: 4043 }).then((ports: any) => {
+            expect(ports).to.exist;
+        });
+    });
+
+    it('restarts after setting ports', function () {
+        return instance.setPorts({ api: 5041, swarm: 4041, gateway: 8040 }, true)
+            .then((ports: any) => {
+                expect(IpfsConnector.getInstance().apiAddress).to.equal('/ip4/127.0.0.1/tcp/5041');
+                expect(ports).to.exist;
+            });
+    });
+
+    it('disables config addresses', function () {
+        return instance.setPorts({ api: false, swarm: false, gateway: false }).then((ports: any) => {
+            expect(instance.config.api).to.not.exist;
+            expect(ports).to.exist;
+        });
+    });
+
+    it('should remove listeners', function () {
+
+        const cb = () => false;
+        IpfsConnector.getInstance().on('stop', () => {
+            return true;
+        });
+        IpfsConnector.getInstance().on('stop', cb);
+        expect(IpfsConnector.getInstance().listenerCount('stop')).to.eql(2);
+        IpfsConnector.getInstance().removeListener('stop', cb);
+        expect(IpfsConnector.getInstance().listenerCount('stop')).to.eql(1);
+        IpfsConnector.getInstance().removeAllListeners('stop');
+        expect(IpfsConnector.getInstance().listenerCount('stop')).to.eql(0);
+    });
+
+    it('Overwrites options', function(){
+       IpfsConnector.getInstance().setOptions({start: false, repo: 'test1'});
+       expect(IpfsConnector.getInstance().config.start).to.be.false;
+    });
+
+    it('doesnt throw when calling sequential stop', function () {
+       const calls = () => {
+           IpfsConnector.getInstance().stop();
+           return IpfsConnector.getInstance().stop();
+       };
+       expect(calls).to.not.throw(Error);
+    });
+
+    after(function (done) {
+        return rimraf(repo, done);
+    });
 });
