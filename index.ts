939b913ae860abf26f66b8c99d8135ee918cec42
Marius Darila <marius.darila@gmail.com>
2017-09-14 14:59:46 +0300

    chore(): update deps

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import * as Promise from 'bluebird';
 
 const symbolEnforcer = Symbol();
 const symbol = Symbol();
-const requiredVersion = '0.25.2';
+const requiredVersion = '0.26.0';
 
 export default class IpfsJsConnector {
 
