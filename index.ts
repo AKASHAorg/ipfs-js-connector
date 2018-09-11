5b9dbf124bc032ea5ac071bd73a4229e4436d965
Marius Darila <marius.darila@gmail.com>
2018-09-11 20:22:11 +0300

    chore(): update js-ipfs

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import * as Promise from 'bluebird';
 
 const symbolEnforcer = Symbol();
 const symbol = Symbol();
-const requiredVersion = '0.31.7';
+const requiredVersion = '0.32.0';
 
 export default class IpfsJsConnector {
 
